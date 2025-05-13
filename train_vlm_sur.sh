b2f6f06d7f82755874168c558f67a38858135fbb
wooseungw <woow070840@gmail.com>
2025-05-14 00:43:49 +0900

    feat: train_vlm_sur.sh 파일 생성 및 초기 설정 추가

---
+++ train_vlm_sur.sh
@@ -0,0 +1,4 @@
+export CUDA_VISIBLE_DEVICES=0
+export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
+export TORCH_ZIPFILE_SERIALIZATION=legacy
+python train_vlm.py --config config/train_vlm_baseline.yaml
