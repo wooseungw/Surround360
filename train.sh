fab7153d2db17c4d2aa9cc990d2b408a85f67210
wooseungw <woow070840@gmail.com>
2025-06-23 11:41:20 +0900

    Refactor code structure for improved readability and maintainability

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
-
+export CUDA_VISIBLE_DEVICES=1
+export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
+export TORCH_ZIPFILE_SERIALIZATION=legacy
 
 python train.py --config config/train_sur.yaml
