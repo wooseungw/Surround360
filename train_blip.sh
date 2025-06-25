e27901aaf782bf9a1456fe66385162c798a61fc6
wooseungw <woow070840@gmail.com>
2025-06-25 18:08:40 +0900

    fix: train_blip.sh에서 --config를 --cfg로 수정

--- train_blip.sh
+++ train_blip.sh
@@ -2,5 +2,5 @@ export CUDA_VISIBLE_DEVICES=1
 export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 export TORCH_ZIPFILE_SERIALIZATION=legacy
 
-python train_blip.py --config config/train.yaml
-python train_blip.py --config config/eval_blip.yaml
+python train_blip.py --cfg config/train.yaml
+python train_blip.py --cfg config/eval_blip.yaml
