ed9d6215dd809b2947fb888e9f0c541fe19eb9b4
wooseungw <woow070840@gmail.com>
2025-07-05 19:20:53 +0900

    fix: tande.sh에서 훈련 스크립트 수정 및 TORCH_ZIPFILE_SERIALIZATION 환경 변수 추가

--- tande.sh
+++ tande.sh
@@ -1,5 +1,8 @@
 export CUDA_VISIBLE_DEVICES=1
 export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
+export TORCH_ZIPFILE_SERIALIZATION=legacy
 
-python train.py --config config/train.yaml
+python train.py --config config/stage1_pretrain.yaml --stage 1
+python train.py --config config/stage2_finetune.yaml --stage 2
+
 python eval.py --config config/eval.yaml
